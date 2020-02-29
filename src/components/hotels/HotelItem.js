76a59a96ad130e9d91123ab815d94416718e0baf
Ida Stensrud <idast3@gmail.com>
2020-02-29 14:14:40 +0100

    Search filter through hotels

--- src/components/hotels/HotelItem.js
+++ src/components/hotels/HotelItem.js
@@ -10,15 +10,16 @@ export default function HotelItem({ hotel }) {
 
     console.log(hotel)
 
-    const { id, establishmentName, imageUrl } = hotel
+    const { id, establishmentName, imageUrl, description, price } = hotel
 
     return (
 
       <div className="hotel">
-      <Card>
+      <Card className="hotel_card">
           <Card.Img variant="top" src={imageUrl} />
           <Card.Body>
-              <Card.Title>{establishmentName}</Card.Title>
+              <Card.Title className="hotel_name" >{establishmentName}</Card.Title>
+              <p className="hotel_price">${price}</p>
               <Link to={`hotel/${id}`} class="btn btn-dark">View more</Link>
           </Card.Body>
       </Card>
--- src/components/hotels/HotelList.js
+++ src/components/hotels/HotelList.js
@@ -12,6 +12,7 @@ import FormControl from "react-bootstrap/FormControl";
 export default function HotelList() {
 
     const [hotels, setHotels] = useState([]);
+    const [filteredCards, setFilteredCards] = useState([]);
 
     const url = BASE_URL + "get-establishments.php";
 
@@ -25,19 +26,45 @@ export default function HotelList() {
         fetchData();
     }, [url]);
 
+    function filterThroughCards(event) {
+        const searchValue = event.target.value.toLowerCase();
+
+        const searchedCards = hotels.filter(function(hotel) {
+            //
+            const lowercaseCardName = hotel.establishmentName.toLowerCase();
+
+            if (lowercaseCardName.startsWith(searchValue)) {
+                return true;
+            } else {
+                return false;
+            }
+        });
+
+        setFilteredCards(searchedCards);
+    }
+
     return (
 
         <div className="hotel-container">
+        <div className="hotel-container_search">
          <h2>Find accommodations</h2>
-        <Form inline className="search">
-            <FormControl type="text" placeholder="Search" className="mr-sm-2" />
+        <Form className="search">
+            <FormControl type="text" placeholder="Search..." className="mr-sm-2 search-input" onChange={event => filterThroughCards(event)}/>
         </Form>
+        </div>
         <Row>
-            {hotels.map(hotel => (
+
+        {filteredCards.map(hotel => {
+            const { id, establishmentName, imageUrl } = hotel;
+
+            return (
               <Col sm={6} md={3} key={hotel.id}>
                 <HotelItem hotel={hotel} key={hotel.id} />
               </Col>
-            ))}
+            );
+        })}
+
+
         </Row>
         </div>
 
